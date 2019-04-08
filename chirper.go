da9f77343ad5d726f8d0adf9e9b3a55b8ed06fc9
Jeremy Kelley <jkelley@carbonblack.com>
2019-04-07 23:22:15 -0500

    formatting

--- chirper.go
+++ chirper.go
@@ -48,21 +48,22 @@ func runChirper(irccon *irc.Connection, c chan string) {
 
 
 func runIrc(ircOpts ircConfig, c chan string) {
-  irccon := irc.IRC(ircOpts.nick, "chirperIRC")
-  irccon.VerboseCallbackHandler = true
-  irccon.Debug = true
-  irccon.UseTLS = ircOpts.ssl
-  irccon.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-  irccon.AddCallback("001", func(e *irc.Event) { irccon.Join(ircOpts.channel) })
-  irccon.AddCallback("366", func(e *irc.Event) {  })
-  err := irccon.Connect(ircOpts.server)
-  if err != nil {
-    fmt.Printf("Err %s", err )
-    return
-  }
-  ircOpts.ircObj = irccon
-  go runChirper(irccon, c)
-  irccon.Loop()
+    irccon := irc.IRC(ircOpts.nick, "chirperIRC")
+    irccon.VerboseCallbackHandler = true
+    irccon.Debug = true
+    irccon.UseTLS = ircOpts.ssl
+    irccon.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+    irccon.AddCallback("001",
+        func(e *irc.Event) { irccon.Join(ircOpts.channel) })
+    irccon.AddCallback("366", func(e *irc.Event) {  })
+    err := irccon.Connect(ircOpts.server)
+    if err != nil {
+        fmt.Printf("Err %s", err )
+        return
+    }
+    ircOpts.ircObj = irccon
+    go runChirper(irccon, c)
+    irccon.Loop()
 }
 
 
@@ -78,33 +79,30 @@ func Chirp(w http.ResponseWriter, r *http.Request) {
         ircChan <- x
     }
     json.NewEncoder(w).Encode("ok")
-    return
 }
 
 
 func runHttpd(apiOpts apiConfig) {
     router := mux.NewRouter()
-
     router.HandleFunc("/chirp", Chirp).Methods("GET")
-
     bindhost := fmt.Sprintf("%s:%d", apiOpts.host, apiOpts.port)
     log.Fatal(http.ListenAndServe(bindhost, router))
 }
 
 
 func setupCfg() {
-
-    flag.StringVar(&ircCfg.server, "server", "", "host:port to connect to" )
-    flag.BoolVar(&ircCfg.ssl, "ssl", true, "ssl to server")
+    // irc related config
+    flag.StringVar(&ircCfg.server, "server", "", "irc host:port to connect to")
+    flag.BoolVar(&ircCfg.ssl, "ssl", true, "irc ssl to server")
     flag.StringVar(&ircCfg.channel, "channel",
-        "#chirper", "channel to connect to" )
+        "#chirper", "irc channel to connect to" )
     flag.StringVar(&ircCfg.nick, "nick",
-        "chirper", "nick for chirper to use" )
+        "chirper", "irc nick for chirper to use" )
 
     flag.StringVar(&apiCfg.host, "host",
-        "localhost", "host to listen on" )
+        "localhost", "http host to listen on" )
     flag.IntVar(&apiCfg.port, "port",
-        8890, "port to listen on" )
+        8890, "http port to listen on" )
 
     flag.Parse()
 }
